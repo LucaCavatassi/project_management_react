e822fa7b8c16394e0be42b8c523333db9ca5a2e9
LucaCavatassi <lucamariacavatassi@gmail.com>
2024-09-12 17:33:33 +0200

    feat: button to add projects

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ function App() {
   return (
     <div className="pt-20 h-screen">
       <Sidebar/>
-      <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1>
     </div>
   );
 }
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,8 +2,9 @@ import classes from "./Sidebar.module.css"
 
 export default function Sidebar(){
     return (
-        <div className={`${classes.sideBg} ${classes.sideMax} h-screen w-1/3`}>
+        <div className={`${classes.sideBg} ${classes.sideMax} h-full w-1/3`}>
             <h1 className={` uppercase text-white text-2xl font-bold pt-14 ps-7`}>Your projects</h1>
+            <button className="px-4 py-2 mt-7 ms-7 text-xs md:text-base rounded-md bg-stone-700 text-stone-400 hover:bg-stone-600 hover:text-stone-100">&#43; Add Project</button>
         </div>
     )
 
