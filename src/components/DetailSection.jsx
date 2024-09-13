2e4315fa7732ee38fc2818e19e676bd94d212268
LucaCavatassi <lucamariacavatassi@gmail.com>
2024-09-13 16:27:27 +0200

    feat: task layout

--- src/components/DetailSection.jsx
+++ src/components/DetailSection.jsx
@@ -13,7 +13,7 @@ export default function DetailSection ({projectDetail}) {
     
 
     return (
-        <div className="pt-10 px-24 flex-grow">
+        <div className="pt-10 px-24 flex-grow h-full">
             <h2 className="text-5xl font-bold text-stone-700 my-4">{projectDetail.title}</h2>
             <p className="text-stone-600 mb-4">{formattedDate}</p>
             <p className="mb-4">{projectDetail.description}</p>
@@ -21,7 +21,18 @@ export default function DetailSection ({projectDetail}) {
             <hr className="border rounded bg-stone-300 h-1" />
 
 
-            <h2 className="text-3xl font-bold text-stone-700 my-4">Tasks</h2>
+            <div>
+                <label htmlFor="tasks" className="text-3xl font-bold text-stone-700 my-4 mb-5 mt6 block">Tasks</label>
+                <input id="tasks" className="w-3/4 p-1 border-b-2 rounded-sm border-stone-300 bg-stone-200 text-stone-600 focus:outline-none focus:border-stone-600" />
+                <button className="text-stone-700 hover:text-green-500 hover:cursor-pointer ms-6">&#43; Add Task</button>
+            </div>
+
+            <div className="mt-8 bg-stone-100 w-full h-64 overflow-auto rounded">
+                <div className="flex justify-between">
+                    <p className="text-stone-800 text-lg my-4 ms-4">{formattedDate}</p>
+                    <button className="text-stone-700 hover:text-red-500 me-8">Clear</button>
+                </div>
+            </div>
 
         </div>
     )
