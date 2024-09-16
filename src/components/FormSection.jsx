bec2930ec49f6ce105ad429355b639811a9a5e64
LucaCavatassi <lucamariacavatassi@gmail.com>
2024-09-16 09:41:41 +0200

    feat: ugly modal showing

--- src/components/FormSection.jsx
+++ src/components/FormSection.jsx
@@ -1,6 +1,7 @@
 import classes from "./MainSection.module.css"
 import logo from "../assets/no-projects.png"
-import { useState } from "react";
+import { useRef, useState } from "react";
+import Modal from "./Modal";
 
 export default function MainSection({showForm, onShow, onFormSubmit}){
     function LoadScreen() {
@@ -19,45 +20,62 @@ export default function MainSection({showForm, onShow, onFormSubmit}){
         const [description, setDescription] = useState("");
         const [date, setDate] = useState("");
 
-        function handleForm() {
-            const formData = { title, description, date };
-            onFormSubmit(formData)
-        }
+        let isEmpty = false;
+        const modal = useRef()
 
+    if (title === "" || description === "" || date === "") {
+        isEmpty = true;
+    } else {
+        isEmpty = false;
+    }
+    
+    function handleForm() {
+        const formData = { title, description, date };
+        onFormSubmit(formData)
+    }
 
+    function handleModal() {
+        modal.current.open();
+    }
 
         return (
-            <div className="flex-grow text-gray-700 mt-28 ps-20 pe-36 flex flex-col">
-                <div className="ms-auto mb-2">
-                    <button className="text-stone-700 hover:text-red-500 me-4">Cancel</button>
-                    <button onClick={handleForm} className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950">Save</button>
+            <>  
+                <Modal ref={modal}> 
+                    <h2>Invalid input!</h2>
+                    <p>Oops....something is missing.</p>
+                    <p>Provide a value for every input field.</p>
+                </Modal>
+                <div className="flex-grow text-gray-700 mt-28 ps-20 pe-36 flex flex-col">
+                    <div className="ms-auto mb-2">
+                        <button onClick={ isEmpty ? handleModal : handleForm} className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950">Save</button>
+                    </div>
+
+                    <form>
+                        <label htmlFor="title" className="text-lg font-bold uppercase text-stone-500">title</label>
+                        <input 
+                            value={title}
+                            onChange={(e) => setTitle(e.target.value)} 
+                            id="title" 
+                            type="text" 
+                            className="w-full p-1 mb-6 border-b-2 rounded-sm border-stone-300 bg-stone-200 text-stone-600 focus:outline-none focus:border-stone-600" />
+
+                        <label htmlFor="description" className="mb-1 mt-8 text-lg font-bold uppercase text-stone-500">description</label>
+                        <textarea 
+                            value={description}
+                            onChange={(e) => setDescription(e.target.value)} 
+                            id="description" 
+                            className="w-full h-24 p-1 mb-5 border-b-2 rounded-sm border-stone-300 bg-stone-200 text-stone-600 focus:outline-none focus:border-stone-600" />
+
+                        <label htmlFor="date" className="mb-1 mt-6 text-lg font-bold uppercase text-stone-500">date</label>
+                        <input 
+                            value={date}
+                            onChange={(e) => setDate(e.target.value)}  
+                            id="date" 
+                            type="date" 
+                            className="w-full p-1 border-b-2 rounded-sm border-stone-300 bg-stone-200 text-stone-600 focus:outline-none focus:border-stone-600" />
+                    </form>
                 </div>
-
-                <form>
-                    <label htmlFor="title" className="text-lg font-bold uppercase text-stone-500">title</label>
-                    <input 
-                        value={title}
-                        onChange={(e) => setTitle(e.target.value)} 
-                        id="title" 
-                        type="text" 
-                        className="w-full p-1 mb-6 border-b-2 rounded-sm border-stone-300 bg-stone-200 text-stone-600 focus:outline-none focus:border-stone-600" />
-
-                    <label htmlFor="description" className="mb-1 mt-8 text-lg font-bold uppercase text-stone-500">description</label>
-                    <textarea 
-                        value={description}
-                        onChange={(e) => setDescription(e.target.value)} 
-                        id="description" 
-                        className="w-full h-24 p-1 mb-5 border-b-2 rounded-sm border-stone-300 bg-stone-200 text-stone-600 focus:outline-none focus:border-stone-600" />
-
-                    <label htmlFor="date" className="mb-1 mt-6 text-lg font-bold uppercase text-stone-500">date</label>
-                    <input 
-                        value={date}
-                        onChange={(e) => setDate(e.target.value)}  
-                        id="date" 
-                        type="date" 
-                        className="w-full p-1 border-b-2 rounded-sm border-stone-300 bg-stone-200 text-stone-600 focus:outline-none focus:border-stone-600" />
-                </form>
-            </div>
+            </>
         )
     }
 
